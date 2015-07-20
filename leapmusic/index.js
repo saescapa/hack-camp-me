eff6912b77385e6af57ebc394596dc40a7c52a8f
saescapa <samuelescapa@hotmail.com>
2015-07-20 23:08:08 +0000

    Leap Music

--- leapmusic/index.js
+++ leapmusic/index.js
@@ -1,49 +1,112 @@
 var cats = {};
 var drums_boxes = {
-  "snare" : {
-    "start" : {
-      x : 250,
-      y : 144
+  snare : {
+    start : {
+      x : 0,
+      y : 0
     },
-    "end" : {
-      x : 133,
-      y : 121
+    end : {
+      x : 200,
+      y : 200
     }
   },
-  "base" : {
-    "x" : 30,
-    "y" : 19
-  }
+  bass : {
+    start : {
+      x : -200,
+      y : 0
+    },
+    end : {
+      x : 0,
+      y : 200
+    }
+  },
+  cymbal : {
+    start : {
+      x : 0,
+      y : 200
+    },
+    end : {
+      x : 200,
+      y : 400
+    }
+  },
 }
 Leap.loop(function(frame) {
   frame.hands.forEach(function(hand, index) {
-    console.log(hand.screenPosition());
-    console.log(hand.stabilizedPalmPosition());
+    //console.log(hand.type);
     var cat = ( cats[index] || (cats[index] = new Cat()) );    
     cat.setTransform(hand.screenPosition(), hand.roll());
+    //console.log(hand.palmPosition[0]);
+    //console.log(hand.palmPosition[1]);
   });
-  if(frame.gestures.length > 0) {
-    frame.gestures.forEach( function(gesture, index) {
-      console.log(frame.gestures[index]);
+  
+  frame.gestures.forEach( function(gesture, index) {
+    if(gesture.type == "screenTap") {
+      console.log("Tap");
       var gesture_handIds = frame.gestures[index].handIds;
       frame.hands.forEach(function(hand, index) {
         var hand = frame.hands[index];
-        if(true) { //Detect the gesture
-          console.log(hand.stabilizedPalmPosition);
-          if (hand.stabilizedPalmPosition[0] < 0) {
-              //Find position.
-              //Check with what drum it matches with
+        if(hand.type == "right") {
+          if (inside(hand.palmPosition[0],hand.palmPosition[1],drums_boxes.snare)) {
+              document.getElementById('snare').play();
+              document.getElementById('snare').currentTime = 0;
+          }
+          else if (inside(hand.palmPosition[0],hand.palmPosition[1],drums_boxes.bass)) {
+              document.getElementById('bass').play();
+              document.getElementById('bass').currentTime = 0;
+          }
+          else if (inside(hand.palmPosition[0],hand.palmPosition[1],drums_boxes.cymbal)) {
+              document.getElementById('cymbal').play();
+              document.getElementById('cymbal').currentTime = 0;
           }
         }
       });
-    });
-  }
+    } else if (gesture.type == "circle" && gesture.state == "start") {
+      console.log("circle");
+      frame.hands.forEach(function(hand, index) {
+        var hand = frame.hands[index];
+        if(hand.type == "right") {
+          if (inside(hand.palmPosition[0],hand.palmPosition[1],drums_boxes.snare)) {
+              loop(document.getElementById('snare_timer').value,false,"snare");
+              timer["snare"] = false;
+          }
+          else if (inside(hand.palmPosition[0],hand.palmPosition[1],drums_boxes.bass)) {
+              loop(document.getElementById('bass_timer').value,false,"bass");
+              timer["bass"] = false;
+          }
+          else if (inside(hand.palmPosition[0],hand.palmPosition[1],drums_boxes.cymbal)) {
+              loop(document.getElementById('cymbal_timer').value,false,"cymbal");
+              timer["cymbal"] = false;
+          }
+        }
+      });
+    } else if (gesture.type == "swipe") {
+      console.log("swipe");
+      frame.hands.forEach(function(hand, index) {
+        var hand = frame.hands[index];
+        if(hand.type == "right") {
+          if (inside(hand.palmPosition[0],hand.palmPosition[1],drums_boxes.snare)) {
+              loop(document.getElementById('snare_timer').value,true,"snare");
+              timer["snare"] = true;
+          }
+          else if (inside(hand.palmPosition[0],hand.palmPosition[1],drums_boxes.bass)) {
+              loop(document.getElementById('bass_timer').value,true,"bass");
+              timer["bass"] = true;
+          }
+          else if (inside(hand.palmPosition[0],hand.palmPosition[1],drums_boxes.cymbal)) {
+              loop(document.getElementById('cymbal_timer').value,true,"cymbal");
+              timer["cymbal"] = true;
+          }
+        }
+      });
+    }
+  });
 }).use('screenPosition', {scale: 0.25});
 
 var Cat = function() {
   var cat = this;
   var img = document.createElement('img');
-  img.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/109794/cat_2.png';
+  img.src = 'https://i.imgur.com/b1FqGt2.png';
   img.style.position = 'absolute';
   img.onload = function () {
     cat.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
@@ -63,6 +126,27 @@ var Cat = function() {
   };
 
 };
+
+function inside (x,y,value) {
+  if(x > value.start.x  && x < value.end.x && y > value.start.y  && y < value.end.y) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var timer = {"snare" : false, "bass" : false, "cymbal" : false};
+function loop(time,end,type) {
+  if(end == false && timer[type] == false) {
+    setTimeout(function () {
+        document.getElementById(type + "_loop").play();
+        document.getElementById(type).currentTime = 0; 
+        loop(time,false,type);
+    }, time*10);
+  } else if(timer[type] == true) {
+    return;
+  }
+}
 
 cats[0] = new Cat();
 
